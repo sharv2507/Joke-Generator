5145a482411f9097892e102e212709c0f72563ac
Sharv Ranjan <54897864+sharv2507@users.noreply.github.com>
2023-08-27 19:12:34 +0530

    The full app is deployed

---
+++ app.js
@@ -0,0 +1,18 @@
+const btnTrigger = document.querySelector("#btn-jokecall");
+const output = document.querySelector(".joke-display");
+
+function printting (_something){
+    output.innerHTML = _something.setup + "<br><br>" +_something.delivery;
+    
+}
+
+const url = "https://v2.jokeapi.dev/joke/Any";
+
+
+btnTrigger.addEventListener("click", ()=> {
+    fetch(url)
+.then(joke => joke.json())
+.then(json => printting(json))
+})
+    
+
